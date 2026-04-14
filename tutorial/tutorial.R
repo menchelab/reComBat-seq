18eebb229fd2dfc5d75ac822910f2d917ed1efb1
dmalzl <UaF20091!>
2026-04-14 14:32:35 +0200

    add tutorial script

---
+++ tutorial/tutorial.R
@@ -0,0 +1,23 @@
+# reading in data from files
+counts <- read.table(
+    'muscular_dystrophy.exp.tsv',
+    sep = '\t',
+    quote = '',
+    header = TRUE,
+    row.names = 'X'
+)
+
+meta <- read.table(
+    'muscular_dystrophy.meta.tsv',
+    sep = '\t',
+    quote = '',
+    header = TRUE,
+    row.names = 'X'
+)
+
+# applying recombatseq correction using disease as wanted covariate
+corrected <- reComBat_seq(
+    t(counts), 
+    batch=meta$sra_study_acc, 
+    wanted.variation=meta['Disease']
+)
